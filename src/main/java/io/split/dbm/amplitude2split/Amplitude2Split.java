874a375adf6d3d0c08b7787c4cd4d14f3cbaa84e
HJewkes <hjewkes@gmail.com>
2021-02-22 23:16:14 -0800

    Updating ReadMe

--- src/main/java/io/split/dbm/amplitude2split/Amplitude2Split.java
+++ src/main/java/io/split/dbm/amplitude2split/Amplitude2Split.java
@@ -6,17 +6,10 @@ import java.util.stream.Stream;
 
 public class Amplitude2Split {
     public static void main(String[] args) {
-        if(args.length != 1) {
-            System.err.println("ERROR - first argument should be configuration file  (e.g. java -jar amplitude2split.jar amplitude2split.config)");
-            System.exit(1);
-        }
-
         try {
-            // Parse config file
-            Configuration config = Configuration.fromFile(args[0]);
-
             System.out.println("INFO - Starting Sync");
 
+            Configuration config = Configuration.fromFile(configFile(args));
             EventsHttpClient eventsClient = new EventsHttpClient(config);
             Stream<Event> events = eventsClient.getEventsFromAmplitude();
             eventsClient.sendEventsToSplitBatched(events);
@@ -28,5 +21,13 @@ public class Amplitude2Split {
             e.printStackTrace(System.err);
             System.exit(1);
         }
+    }
+
+    private static String configFile(String[] args) {
+        if(args.length != 1) {
+            System.err.println("ERROR - first argument should be configuration file  (e.g. java -jar amplitude2split.jar amplitude2split.config)");
+            System.exit(1);
+        }
+        return args[0];
     }
 }

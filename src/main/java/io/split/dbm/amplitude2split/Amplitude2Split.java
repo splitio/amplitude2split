448300b0e799a226ef7e56a4b27c95ddae971dd5
HJewkes <hjewkes@gmail.com>
2021-02-22 23:02:17 -0800

    Clarifying functions

--- src/main/java/io/split/dbm/amplitude2split/Amplitude2Split.java
+++ src/main/java/io/split/dbm/amplitude2split/Amplitude2Split.java
@@ -5,28 +5,28 @@ import java.time.Instant;
 import java.util.stream.Stream;
 
 public class Amplitude2Split {
-	public static void main(String[] args) {
-		if(args.length != 1) {
-			System.err.println("ERROR - first argument should be configuration file  (e.g. java -jar amplitude2split.jar amplitude2split.config)");
-			System.exit(1);
-		}
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.err.println("ERROR - first argument should be configuration file  (e.g. java -jar amplitude2split.jar amplitude2split.config)");
+            System.exit(1);
+        }
 
-		try {
-			// Parse config file
-			Configuration config = Configuration.fromFile(args[0]);
+        try {
+            // Parse config file
+            Configuration config = Configuration.fromFile(args[0]);
 
-			System.out.println("INFO - Starting Sync");
+            System.out.println("INFO - Starting Sync");
 
-			EventsHttpClient eventsClient = new EventsHttpClient(config);
-			Stream<Event> events = eventsClient.getEventsFromAmplitude();
-			eventsClient.sendEventsToSplitBatched(events);
+            EventsHttpClient eventsClient = new EventsHttpClient(config);
+            Stream<Event> events = eventsClient.getEventsFromAmplitude();
+            eventsClient.sendEventsToSplitBatched(events);
 
-			long elapsedTime = Duration.between(config.jobStart, Instant.now()).getSeconds();
-			System.out.printf("INFO - Finished sync: elapsed=%ss %n", elapsedTime);
-		} catch(Exception e) {
-			System.err.printf("ERROR - Exiting with error: %s %n", e.getMessage());
-			e.printStackTrace(System.err);
-			System.exit(1);
-		}
-	}
+            long elapsedTime = Duration.between(config.jobStart, Instant.now()).getSeconds();
+            System.out.printf("INFO - Finished sync: elapsed=%ss %n", elapsedTime);
+        } catch(Exception e) {
+            System.err.printf("ERROR - Exiting with error: %s %n", e.getMessage());
+            e.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
 }

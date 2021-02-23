ef891b6e95cf80d528f9a35e27312a0504130fce
HJewkes <hjewkes@gmail.com>
2021-02-22 23:11:43 -0800

    Updating ReadMe

--- src/main/java/io/split/dbm/amplitude2split/Configuration.java
+++ src/main/java/io/split/dbm/amplitude2split/Configuration.java
@@ -13,17 +13,16 @@ import java.util.Set;
 public class Configuration {
     public final Instant jobStart = Instant.now();
     public final Duration fetchWindow = Duration.ofHours(1);
-
-    @SerializedName("amplitude.api.key") public String amplitudeApiKey;
-    @SerializedName("amplitude.api.secret") public String amplitudeApiSecret;
-    @SerializedName("split.api.key") public String splitApiKey;
-    @SerializedName("trafficType") public String splitTrafficType;
-    @SerializedName("environment") public String splitEnvironment;
-    @SerializedName("eventPrefix") public String eventTypePrefix;
-    @SerializedName("key") public String userIdField;
-    @SerializedName("value") public String valueField;
-    @SerializedName("batchSize") public int batchSize;
-    @SerializedName("string.property.keys") public Set<String> propertyFields;
+    public String amplitudeApiKey;
+    public String amplitudeApiSecret;
+    public String splitApiKey;
+    public String splitTrafficType;
+    public String splitEnvironment;
+    public String eventTypePrefix;
+    public String userIdField;
+    public String valueField;
+    public int batchSize;
+    public Set<String> propertyFields;
 
     public static Configuration fromFile(String configFilePath) throws IOException {
         String configContents = Files.readString(Paths.get(configFilePath));
